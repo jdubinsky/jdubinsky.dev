b6114af9ea5c0dbfb63e518e5e94f0a189e2ba8e
Jacob Dubinsky <jacobmdubinsky@gmail.com>
2019-09-21 19:39:32 -0400

    init terraform files use serverless express library

---
+++ infra/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "jdubinsky-dev-lambda.zip"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "jdubinsky-dev"
+
+  filename         = "${data.archive_file.zip.output_path}"
+  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+
+  role    = "${aws_iam_role.iam_for_lambda.arn}"
+  handler = "index.handler"
+  runtime = "nodejs10.16.3"
+
+  environment {
+    variables = {
+      APP_PORT = 8080
+      NODE_ENV = "production"
+    }
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = process.env.APP_PORT;
 
 app.use("/static", express.static(path.join(__dirname, "static")));
 
@@ -10,7 +9,5 @@ app.get("/", (request: express.Request, response: express.Response) => {
   const indexPath = path.join(__dirname, "/index.html");
   return response.sendFile(indexPath);
 });
-
-app.listen(port, () => console.log(`Now listening on port ${port}`));
 
 export default app;
