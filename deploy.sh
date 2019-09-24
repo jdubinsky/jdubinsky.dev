54bd9aa87df141091857c87c6c8f968e5b76fd23
Jacob Dubinsky <jacobmdubinsky@gmail.com>
2019-09-23 21:38:04 -0400

    deploy script in CI

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,16 @@
 pushd src
 
 # build js
+echo "Compiling..."
 npx tsc
 
 # install dependencies (prod only)
+echo "Copying files..."
 cp package*.json build/
 cp index.html build/
 
 pushd build
+echo "Installing packages..."
 npm install --only=prod
 popd
 
@@ -18,6 +21,10 @@ popd
 # deploy new lambda zip
 
 pushd infra
+echo "Deploying to AWS..."
 # TODO: check that terraform plan output is only lambda
-terraform apply -input=false
+# terraform apply -input=false
+terraform plan -input=false
 popd
+
+echo "Done!"
