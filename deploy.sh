737f4bb7c703b7f731c3a2963230d6928821170c
Jacob Dubinsky <jacobmdubinsky@gmail.com>
2019-09-23 22:04:24 -0400

    setup terraform before running

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,26 @@
 
 pushd src
 echo "Installing dev packages..."
+pwd
 npm install
 
 # build js
 echo "Compiling..."
+pwd
 npx tsc
 
 # install dependencies (prod only)
 echo "Copying files..."
+pwd
+ls
 cp package*.json build/
 cp index.html build/
 
 pushd build
+pwd
 echo "Installing prod packages..."
 npm install --only=prod
+ls
 popd
 
 popd
@@ -23,6 +29,8 @@ popd
 # deploy new lambda zip
 
 pushd infra
+pwd
+ls
 echo "Deploying to AWS..."
 # TODO: check that terraform plan output is only lambda
 # terraform apply -input=false
